49f898388949bcf4fbed39942ce915f8240ac2b2
daniele-pipino <Danielemacpipino@icloud.com>
2021-10-01 17:17:42 +0200

    header logo + navbar completi

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,28 @@
+<header>
+    {{-- top header --}}
+    <div class="top-header"></div>
+        <div class="container">
+            <div class="main-header d-flex">
+                 {{-- logo --}}
+            <div class="header-logo h-100">
+                <img src="{{ asset('images/dc-logo.png') }}" alt="">
+            </div>
+            {{-- navbar --}}
+            <div class="main-nav">
+                <ul>
+                    <li><a href="">CHARACTERS</a></li>
+                    <li><a href="">COMICS</a></li>
+                    <li><a href="">MOVIES</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">GAMES</a></li>
+                    <li><a href="">COLLECTIBLES</a></li>
+                    <li><a href="">VIDEOS</a></li>
+                    <li><a href="">FANS</a></li>
+                    <li><a href="">NEWS</a></li>
+                    <li><a href="">SHOP</a></li>
+                </ul>
+            </div>
+            {{-- input search --}}
+            </div>
+        </div>
+</header>
